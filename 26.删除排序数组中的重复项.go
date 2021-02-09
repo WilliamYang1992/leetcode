5f9d596eb59fb09abeb10d17dd27f6a9a8f32095
WilliamYang1992 <505741310@qq.com>
2021-02-10 00:28:09 +0800

    26

---
+++ 26.删除排序数组中的重复项.go
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=golang
+ *
+ * [26] 删除排序数组中的重复项
+ */
+
+// @lc code=start
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[i] != nums[j] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	return i + 1
+}
+
+// @lc code=end
